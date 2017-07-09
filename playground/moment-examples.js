66eba9cbc920e10ad0878172380d94385ffc5348
Amaro <amarito93@gmail.com>
2017-07-09 08:55:07 -0400

    checking heroku log

--- playground/moment-examples.js
+++ playground/moment-examples.js
@@ -10,3 +10,5 @@ let currentMoment = moment.unix(timestamp);
 console.log("Current moment", currentMoment.format('MMM D, YYYY @ h:mm a'));
 
 console.log("Current date", moment().format('MMMM Do, YYYY @ hh:mm A'));
+
+console.log('jajaja');
